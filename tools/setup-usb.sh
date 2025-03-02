c7c7680693ca964c654bd70d80cb46081263c86a
jake <bigjake2s@gmail.com>
2025-03-01 19:51:20 -0500

    feat: USB boot drive setup tool

---
+++ tools/setup-usb.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$2" ]; then
+    echo "Usage: $0 path/to/image.iso /dev/sdX1 [/dev/sdX1 ...]"
+    exit 1
+fi
+
+IMAGE_PATH="$1"
+IMAGE_FILE=$(basename "$IMAGE_PATH")
+DISKS=( ${@:2} )
+VENTOY_CONFIG=/tmp/ventoy.json
+MOUNT_DIR=/tmp/ventoy-mnt
+
+cat <<EOF > $VENTOY_CONFIG
+{
+  "control": [
+    { "VTOY_DEFAULT_IMAGE": "/$IMAGE_FILE" }
+  ]
+}
+EOF
+
+mkdir -p "$MOUNT_DIR"
+
+for I in ${!DISKS[@]}; do
+    DISK=${DISKS[$I]}
+    mount $DISK "$MOUNT_DIR"
+    cp -v "$IMAGE_PATH" "$MOUNT_DIR"
+    mkdir -p "$MOUNT_DIR/ventoy"
+    cp -v "$VENTOY_CONFIG" "$MOUNT_DIR/ventoy"
+    umount "$MOUNT_DIR"
+done
+
+rmdir "$MOUNT_DIR"
