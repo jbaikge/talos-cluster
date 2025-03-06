e0b6c678c02a298be5395ba19849532e862c7f0d
jake <bigjake2s@gmail.com>
2025-03-06 12:57:46 -0500

    fix: removing cluster subnet config

--- main.tf
+++ main.tf
@@ -47,7 +47,6 @@ resource "talos_machine_configuration_apply" "worker" {
       install_disk = each.value.install_disk
     }),
     templatefile("${path.module}/templates/pull-through-cache.yaml", {}),
-    templatefile("${path.module}/templates/cluster-subnet.yaml", {}),
   ]
 }
 
